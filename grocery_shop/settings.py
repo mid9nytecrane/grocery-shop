4f96d73cec8cf01fe73fa8dd82b171f9c4473a48
mid9nytecrane <sheriffsakara41@gmail.com>
2025-10-27 11:24:07 +0000

    configuration for vercel deployment

--- grocery_shop/settings.py
+++ grocery_shop/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app", "127.0.0.1", ".now.sh"]
 
 
 # Application definition
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'grocery_shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-if os.environ.get("VERCEL"):
+if DEBUG == "False":
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
